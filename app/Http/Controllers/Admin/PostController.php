4ee084911db52fcb88c64b127a54ee6b27f0b6f8
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-06-21 16:35:36 +0200

    live

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all()->sortDesc();
+        //dd($posts);
+        return view('admin.posts.index',compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  App\Http\Requests\PostRequest;  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        $validate_data = $request->validated();
+        $slug = Str::slug($request->title,'-');
+        $validate_data['slug'] =$slug;
+        Post::create($validate_data);
+        return redirect()->route('admin.posts.index')->with('message','Post Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  App\Http\Requests\PostRequest;  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Auth::routes();
+
+
+Route::middleware('auth')
+->prefix('admin')
+->namespace('Admin')
+->name('admin.')
+->group(function ()
+{
+    //Admin Dashbors
+    Route::get('/', 'HomeController@index')->name('dashboard'); //admin.dashboard
+    //Admin Posts
+    route::resource('posts','PostController');
+
+});
+
+
+
+
+
+
+
+
+
+/* DEVE ESSERE SEMPRE L'ULTIMA */
+Route::get("{any?}", function ()
+{
+    return view('guest.home');
+})->where('any','.*');
